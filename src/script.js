7698f02de9163383659156bf949d7df288862a9b
github username <github email address>
2020-05-04 18:17:05 -0400

    cleaned up code

--- src/script.js
+++ src/script.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // imageIcon()
     // sounds()
     imagePlayAudion()
-
-    setVolume()
     loginSetup()
 
 
@@ -13,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // // makepic()
     makeCanvas()
     ambientIconArray()
-volume()
+    volume()
 
 })
     // "images/cafe.png",
@@ -21,36 +19,35 @@ volume()
     // "images/night.png",
     // "images/fire.png",
     // "images/road.png",
-    // "images/wind .png"
+    // "images/wind.png"
 function ambientIconArray(){ 
-let ambientIcon= [{
-    image:"images/bird.png",
-    audio:"audio/birds.fade.ogg",
+    let ambientIcon = [{
+        image:"images/bird.png",
+        audio:"audio/birds.fade.ogg",
+        },
+        {
+        image:"images/rainy.png",
+        audio:"audio/rain_1.ogg",
+        },
 
-    },
-    {
-    image:"images/rainy.png",
-    audio:"audio/rain_1.ogg",
-    },
-    {
-     audio:"audio/thunder.fade.ogg",
-     image:"images/thunder.png",
+        {
+        audio:"audio/thunder.fade.ogg",
+        image:"images/thunder.png",
+        },
 
-    },
-    {
+        {
         image:"images/wave.png",
         audio:"audio/River-sounds.mp3"
-    
         },
-]
+    ]
 
-ambientIcon.forEach((item, i) => {
-    item.id = i + 1;
+    ambientIcon.forEach((item, i) => {
+        item.id = i + 1;
 
-    loadImageIcon(item)
-  });
-  
-  console.log(ambientIcon);
+        loadImageIcon(item)
+    });
+
+    console.log(ambientIcon);
 }
 // //==================================================================================
 
@@ -60,14 +57,13 @@ function loadImageIcon(item){
     iconDiv.dataset.id = item.id
 
     iconDiv.innerHTML=`
-    <span>
-   <img class="icon" src=${item.image}>
-   <audio loop><source src=${item.audio}></audio>
-   <input type="range" class="volumeSlider" min="0" max="1" step="0.01" style="cursor: pointer;"><br>
-   </span>
+        <span>
+        <img class="icon" src=${item.image}>
+        <audio loop><source src=${item.audio}></audio>
+        <input type="range" class="volumeSlider" min="0" max="1" step="0.01" style="cursor: pointer;"><br>
+        </span>
     `
     controlsdiv.appendChild(iconDiv)
-
 }
 
 
@@ -77,9 +73,8 @@ function loadImageIcon(item){
 function imagePlayAudion() {
     let img = document.getElementsByClassName("icon") 
     let audio = document.getElementsByTagName("audio") 
- 
 
-  
+
     document.addEventListener("click", (event) => {
         if (event.target === img[0]) {
             togglePlay(audio[0])   
@@ -93,8 +88,6 @@ function imagePlayAudion() {
         if (event.target === img[3]) {
             togglePlay(audio[3])   
         }
-
-
     })
 }
 
@@ -116,22 +109,22 @@ function togglePlay(sound) {
 
 
 function volume(){
-let controlsdiv = document.getElementById("controls")  
+    let controlsdiv = document.getElementById("controls")  
 
     controlsdiv.addEventListener("input",function(event){
         let slider=document.getElementsByClassName("volumeSlider")
         let audio = document.getElementsByTagName("audio") 
 
-        if(event.target===slider[0]){
+        if(event.target===slider[0]) {
             audio[0].volume=event.target.value    
         }
-        if(event.target===slider[1]){
+        if(event.target===slider[1]) {
             audio[1].volume=event.target.value    
         }
-        if(event.target===slider[2]){
+        if(event.target===slider[2]) {
             audio[2].volume=event.target.value    
         }
-        if(event.target===slider[3]){
+        if(event.target===slider[3]) {
             audio[3].volume=event.target.value    
         }
 
