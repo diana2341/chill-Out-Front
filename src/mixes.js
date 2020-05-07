9227d369bf3d5ba0c48774961ead83075616f6be
github username <github email address>
2020-05-07 01:25:17 -0400

    added the saveNewMix form to my test_script.js file

--- src/mixes.js
+++ src/mixes.js
@@ -1,24 +1,33 @@
-	// click save 
-// capture volume number of each slider 
-// keys of body need to correspond to backend table 
-// post the data to our mix table 
-// that creates new mix record (instance)
+function renderMixForm() {
+  let form_container = document.getElementById("save-mix-form");
 
-// click load 
-// look the mix_id - bind the mix-id to the dom NEED TO KNOW MIX ID
-// like a collection_select  - value= mix_id  // or it shows up on page with a play button. // store mix_id on the button somewhere
-// fetch the mix record from backend - comes in as json 
-// run volume adjuster method on click of play. would be in the .then callback. 
+  let form = document.createElement("form")
+  form.id = "mix-form"
+
+  form.innerHTML = `
+
+    <label>Mix name: </label>
+    <input id="mix-title-input" type="text" name="mix-name">
+    <input type="submit" placeholder="your mix name here">
+  `
+  form_container.append(form)
+}
+
+//
+//
 
 function saveNewMix() {
   let saveBtn = document.getElementById("save")
 
   saveBtn.addEventListener("click", (e) => {
+
     let audio_collection = document.querySelectorAll('audio')
     const user_id = parseInt(document.querySelector('.left-container').dataset.userId, 10)
+    let mix_name = document.getElementById('mix-title-input').value
+    
 
     let new_mix = {
-      mix_name : "placeholder",
+      mix_name : mix_name,
       user_id : user_id,
     }
 
@@ -28,8 +37,8 @@ function saveNewMix() {
       let volume_input = parseFloat(audio.parentNode.children[2].value)
 
       Object.assign(new_mix, {[audio_name] : volume_input})
+
     })
-    // console.log("an object perhaps====",new_mix)
 
     fetch(`${url}/mixes`, {
       method: 'POST',
@@ -43,3 +52,16 @@ function saveNewMix() {
     .catch(err => console.log("error:", err))
   })
 } 
+
+
+// click save 
+// capture volume number of each slider 
+// keys of body need to correspond to backend table 
+// post the data to our mix table 
+// that creates new mix record (instance)
+
+// click load 
+// look the mix_id - bind the mix-id to the dom NEED TO KNOW MIX ID
+// like a collection_select  - value= mix_id  // or it shows up on page with a play button. // store mix_id on the button somewhere
+// fetch the mix record from backend - comes in as json 
+// run volume adjuster method on click of play. would be in the .then callback. 
